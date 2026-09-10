7778d4dc4bd5ea112ebd6605d0ae1cc466073e3f
kudabesi2016-eng <kudabesi2016@gmail.com>
2026-09-10 20:19:00 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://zaqdjyhosksbdlafhluc.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_v9HwG1jpZXKeo2MYrWmGdg_VO4Or8yt";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
