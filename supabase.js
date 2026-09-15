520884c2cb1586eddbdb187a2e08d0577427d23d
kudabesi2016-eng <kudabesi2016@gmail.com>
2026-09-15 18:29:55 +0700

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,10 +1,4 @@
-const SUPABASE_URL = "https://zaqdjyhosksbdlafhluc.supabase.co";
-
-const SUPABASE_KEY =
-  "sb_publishable_v9HwG1jpZXKeo2MYrWmGdg_VO4Or8yt";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const SUPABASE_URL = 'https://zaqdjyhosksbdlafhluc.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_v9HwG1jpZXKeo2MYrWmGdg_VO4Or8yt';
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 window.supabaseClient = supabaseClient;
